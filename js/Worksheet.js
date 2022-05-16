2047e60741ca4a135fde33c17e474242b3bfaaf7
dkrasner <krasnerdaniel@gmail.com>
2022-05-16 12:37:17 +0200

    styling for worksheet
    adding cell styling for the worksheet sheet
    adding a randomized palette selection for the worksheet and sheet

--- js/Worksheet.js
+++ js/Worksheet.js
@@ -43,12 +43,84 @@ my-grid {
     width: 100%;
     height: 100%;
 }
+
+
+.spreadsheet.editing-cell .view-cell:not(.editing) {
+    opacity: 0.2;
+    transition: opacity 0.2s linear;
+}
+
+.view-cell.editing {
+    transform: scale(1.5);
+    box-shadow: 0px 0px 5px 10px rgba(200, 200, 200, 0.4);
+    transition: transform 0.2s linear; box-shadow 0.2s linear;
+}
+
+.in-locked-row {
+    background-color: rgba(150, 150, 150, 0.4);
+}
+.in-locked-column {
+    background-color: rgba(150, 150, 150, 0.4);
+}
+.view-cell {
+    background-color: transparent;
+    color: var(--palette-black);
+    border-right: 2px solid rgba(200, 200, 200, 0.5);
+    border-bottom: 2px solid rgba(200, 200, 200, 0.5);
+    border-top: 2px solid transparent;
+    border-left: 2px solid transparent;
+}
+
+.view-cell:last-child {
+    border: 2px solid transparent;
+}
+
+#button-area {
+    margin-top: 30px;
+}
+
+.sheet-move-button {
+    display: block;
+    font-size: 3rem;
+}
+
+.selector-cursor {
+    border: 2px solid red;
+}
+
+.selector-anchor {
+    border: 2px solid orange;
+}
+
+.in-selection {
+    background-color: rgba(255, 50, 50, 0.1);
+}
+.selection-top-border {
+    border-top: 2px solid red;
+}
+.selection-bottom-border {
+    border-bottom: 2px solid red;
+}
+.selection-left-border {
+    border-left: 2px solid red;
+}
+.selection-right-border {
+    border-right: 2px solid red;
+}
+
 </style>
 <div id="header-bar">THE BAR</div>
 <div id="sheet-container">
     <my-grid expands=true columns=5 rows=10></my-grid>
 </div>
 `;
+
+// a few palette combo options for the sheet + bar area
+const paletteCombinations = [
+    {this: 'var(--palette-orange)', sheet: 'var(--palette-beige)'},
+    {this: 'var(--palette-lightblue)', sheet: 'var(--palette-cyan)'},
+    {this: 'var(--palette-cyan)', sheet: 'var(--palette-lightblue)'},
+]
 
 class Worksheet extends HTMLElement {
     constructor(){
@@ -60,6 +132,9 @@ class Worksheet extends HTMLElement {
             this.template.content.cloneNode(true)
         );
 
+        // generate a random palette for the worksheet
+        this.palette = paletteCombinations[Math.floor(Math.random() * paletteCombinations.length)];
+
         // bind methods
         this.onMouseMoveInBar = this.onMouseMoveInBar.bind(this);
         this.onMouseDownInBar = this.onMouseDownInBar.bind(this);
@@ -67,6 +142,9 @@ class Worksheet extends HTMLElement {
     }
 
     connectedCallback(){
+        // set the palette
+        this.style.backgroundColor = this.palette.this;
+        this.shadowRoot.querySelector('my-grid').style.backgroundColor = this.palette.sheet;
         // add event listeners
         const bar = this.shadowRoot.querySelector('#header-bar');
         bar.addEventListener("mousedown", this.onMouseDownInBar);
