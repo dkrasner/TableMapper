9cb3ee9a6a313dc6f10d13e437d532a4082d63dd
dkrasner <krasnerdaniel@gmail.com>
2022-10-30 21:01:48 +0100

    adding arithmetic grammar,commands and tests

--- tests/interpreter-tests.js
+++ tests/interpreter-tests.js
@@ -29,6 +29,18 @@ describe("Interpreter Tests", () => {
         ["a0", "b0", "c0"],
         ["a1", "b1", "c1"]
     ];
+    const numericDataArray = [
+        [1, 2, 3],
+        [4, 5, 6]
+    ];
+    const numericDataDict = {
+        "0,0": 1,
+        "1,0": 2,
+        "2,0": 3,
+        "0,1": 4,
+        "1,1": 5,
+        "2,1": 6,
+    };
     before(() => {
         sourceWS = document.createElement('work-sheet');
         anotherSourceWS = document.createElement('work-sheet');
@@ -185,6 +197,153 @@ describe("Interpreter Tests", () => {
         });
     });
 
+    describe("Running arithmetic commands test", () => {
+        it("Can clear all Worksheet data", () => {
+            sourceWS.onErase();
+            expect(sourceWS.sheet.dataFrame.store).to.eql({});
+            targetWS.onErase();
+            expect(targetWS.sheet.dataFrame.store).to.eql({});
+        });
+        it("Can setup source and callstack", () => {
+            sourceWS.sheet.dataFrame.loadFromArray(numericDataArray);
+            expect(sourceWS.sheet.dataFrame.store).to.eql(numericDataDict);
+            const instructions = [
+                [`${sourceWS.id}!(0,0):(2,1)`, `${targetWS.id}!(0,0):(0,0)`, "sum()"]
+            ]
+            callstack.load(instructions);
+            expect(callstack.stack).to.eql(instructions);
+        });
+        it("Sum", () => {
+            callstack.run();
+            let sum = 0;
+            numericDataArray.forEach((row) => {
+                sum += row.reduce((a, b) => a + b);
+            });
+            expect(targetWS.sheet.dataFrame.store).to.eql({"0,0": sum});
+            targetWS.onErase();
+            expect(targetWS.sheet.dataFrame.store).to.eql({});
+        });
+        it("Sum can be NaN", () => {
+            sourceWS.sheet.dataFrame.loadFromArray(dataArray);
+            callstack.run();
+            expect(targetWS.sheet.dataFrame.store).to.eql({"0,0": NaN});
+            targetWS.onErase();
+            expect(targetWS.sheet.dataFrame.store).to.eql({});
+            sourceWS.sheet.dataFrame.loadFromArray(numericDataArray);
+            expect(sourceWS.sheet.dataFrame.store).to.eql(numericDataDict);
+        });
+        it("Average", () => {
+            const instructions = [
+                [`${sourceWS.id}!(0,0):(2,1)`, `${targetWS.id}!(0,0):(0,0)`, "average()"]
+            ]
+            callstack.load(instructions);
+            callstack.run();
+            let ave = 0;
+            let counter = 0;
+            numericDataArray.forEach((row) => {
+                ave += row.reduce((a, b) => a + b);
+                counter += row.length;
+            });
+            ave = ave / counter;
+            expect(targetWS.sheet.dataFrame.store).to.eql({"0,0": ave});
+            targetWS.onErase();
+            expect(targetWS.sheet.dataFrame.store).to.eql({});
+        });
+        it("Average can be NaN", () => {
+            sourceWS.sheet.dataFrame.loadFromArray(dataArray);
+            callstack.run();
+            expect(targetWS.sheet.dataFrame.store).to.eql({"0,0": NaN});
+            targetWS.onErase();
+            expect(targetWS.sheet.dataFrame.store).to.eql({});
+            sourceWS.sheet.dataFrame.loadFromArray(numericDataArray);
+            expect(sourceWS.sheet.dataFrame.store).to.eql(numericDataDict);
+        });
+        it("Max", () => {
+            const instructions = [
+                [`${sourceWS.id}!(0,0):(2,1)`, `${targetWS.id}!(0,0):(0,0)`, "max()"]
+            ]
+            callstack.load(instructions);
+            callstack.run();
+            let max = numericDataArray[0][0];
+            numericDataArray.forEach((row) => {
+                row.forEach((v) => {
+                    if (v > max){
+                        max = v;
+                    }
+                })
+            });
+            expect(targetWS.sheet.dataFrame.store).to.eql({"0,0": max});
+            targetWS.onErase();
+            expect(targetWS.sheet.dataFrame.store).to.eql({});
+        });
+        it("Max can be NaN", () => {
+            sourceWS.sheet.dataFrame.loadFromArray(dataArray);
+            callstack.run();
+            expect(targetWS.sheet.dataFrame.store).to.eql({"0,0": NaN});
+            targetWS.onErase();
+            expect(targetWS.sheet.dataFrame.store).to.eql({});
+            sourceWS.sheet.dataFrame.loadFromArray(numericDataArray);
+            expect(sourceWS.sheet.dataFrame.store).to.eql(numericDataDict);
+        });
+        it("Min", () => {
+            const instructions = [
+                [`${sourceWS.id}!(0,0):(2,1)`, `${targetWS.id}!(0,0):(0,0)`, "min()"]
+            ]
+            callstack.load(instructions);
+            callstack.run();
+            let min = numericDataArray[0][0];
+            numericDataArray.forEach((row) => {
+                row.forEach((v) => {
+                    if (v < min){
+                        min = v;
+                    }
+                })
+            });
+            expect(targetWS.sheet.dataFrame.store).to.eql({"0,0": min});
+            targetWS.onErase();
+            expect(targetWS.sheet.dataFrame.store).to.eql({});
+        });
+        it("Min can be NaN", () => {
+            sourceWS.sheet.dataFrame.loadFromArray(dataArray);
+            callstack.run();
+            expect(targetWS.sheet.dataFrame.store).to.eql({"0,0": NaN});
+            targetWS.onErase();
+            expect(targetWS.sheet.dataFrame.store).to.eql({});
+            sourceWS.sheet.dataFrame.loadFromArray(numericDataArray);
+            expect(sourceWS.sheet.dataFrame.store).to.eql(numericDataDict);
+        });
+        it("Median 1", () => {
+            const numericDataArray = [
+                [1, 2, 3],
+                [4, 5, 6]
+            ];
+            sourceWS.sheet.dataFrame.loadFromArray(numericDataArray);
+            const instructions = [
+                [`${sourceWS.id}!(0,0):(2,1)`, `${targetWS.id}!(0,0):(0,0)`, "median()"]
+            ]
+            callstack.load(instructions);
+            callstack.run();
+            expect(targetWS.sheet.dataFrame.store).to.eql({"0,0": 3.5});
+            targetWS.onErase();
+            expect(targetWS.sheet.dataFrame.store).to.eql({});
+        });
+        it.skip("Median 2", () => {
+            const numericDataArray2 = [
+                [1, 2, 3],
+                [4, 5, 6],
+                [7, 8, 9]
+            ];
+            sourceWS.sheet.dataFrame.loadFromArray(numericDataArray2);
+            const instructions = [
+                [`${sourceWS.id}!(0,0):(2,3)`, `${targetWS.id}!(0,0):(0,0)`, "median()"]
+            ]
+            callstack.load(instructions);
+            callstack.run();
+            expect(targetWS.sheet.dataFrame.store).to.eql({"0,0": 5});
+            targetWS.onErase();
+            expect(targetWS.sheet.dataFrame.store).to.eql({});
+        });
+    });
     after(() => {
         sourceWS.remove();
         targetWS.remove();
